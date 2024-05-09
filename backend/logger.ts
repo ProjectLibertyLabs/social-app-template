b64907dd5e0aa827093852c020297e6c3cea1026
Joe Caputo <joseph.caputo@unfinished.com>
2024-05-09 13:19:09 -0400

    feat: swap pino for morgan to enable full logging throught app

---
+++ backend/logger.ts
@@ -0,0 +1,21 @@
+import pino from 'pino';
+import * as Config from './config/config';
+
+// The logger may be invoked like so:
+//  logger.<method>(obj, [msg], [msg params])
+//  logger.<method>(msg, [msg params]);
+// where:
+//   - msg = printf-style message string
+//   - msg-params = replacement parameters for msg string (%o, %s, %d, etc)
+const logger = pino({
+  level: Config.instance().debug ? 'trace' : 'info',
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      colorize: true,
+      async: true,
+    }
+  }
+})
+
+export default logger;
