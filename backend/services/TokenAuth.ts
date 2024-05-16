516134c684c290e55e435b42762574aaca0a87d8
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2024-05-16 16:02:54 -0500

    revert changes to token auth

--- backend/services/TokenAuth.ts
+++ backend/services/TokenAuth.ts
@@ -1,98 +1,95 @@
 import { randomUUID } from 'crypto';
 import { getApi } from './frequency.js';
+import { NextFunction, Request, Response } from 'express';
 import { HttpStatusCode } from 'axios';
-import { Request, Response, NextFunction } from 'express';
+import { AccountService } from './AccountService.js';
 import logger from '../logger';
 
-export interface RequestAccount {
-  publicKey: string;
-  msaId?: string;
+export type RequestAccount = { publicKey: string; msaId?: string };
+
+// uuid auth token to Public Key
+const authTokenRegistry: Map<string, RequestAccount> = new Map();
+
+type CacheData = { msaId: string; added: Date };
+const cachePublicKeys: Map<string, CacheData> = new Map();
+
+/**
+ * Creates an authentication token for the given public key.
+ * @param publicKey - The public key associated with the token.
+ * @returns The generated authentication token.
+ */
+export function createAuthToken(publicKey: string): string {
+  const uuid = randomUUID();
+  authTokenRegistry.set(uuid, { publicKey });
+  return uuid;
 }
 
-export class TokenManager {
-  private authTokenRegistry: Map<string, RequestAccount> = new Map();
-  private cachePublicKeys: Map<string, { msaId: string; added: Date }> = new Map();
-
-  /**
-   * Creates an authentication token for the given public key.
-   * @param publicKey - The public key associated with the token.
-   * @returns The generated authentication token.
-   */
-  createAuthToken(publicKey: string): string {
-    const uuid = randomUUID();
-    this.authTokenRegistry.set(uuid, { publicKey });
-    return uuid;
-  }
-
-  /**
-   * Revokes an authentication token.
-   * @param token - The authentication token to revoke.
-   */
-  revokeAuthToken(token: string): void {
-    this.authTokenRegistry.delete(token);
-  }
-
-  /**
-   * Retrieves the account associated with the provided authentication token.
-   * @param token - The authentication token.
-   * @returns A Promise that resolves to the account associated with the token, or null if no account is found.
-   */
-  async getAccountFromAuth(token: string): Promise<null | RequestAccount> {
-    const account = this.authTokenRegistry.get(token);
-    if (!account) return null;
-    if (account.msaId) return account;
-
-    // Cache that msaId if we can...
-    account.msaId = (await this.getMsaByPublicKey(account.publicKey)) || undefined;
-    this.authTokenRegistry.set(token, account);
-    return account;
-  }
-
-  /**
-   * Retrieves the MSA ID associated with the given public key.
-   * @param publicKey - The public key for which to retrieve the MSA ID.
-   * @returns A Promise that resolves to the MSA ID as a string, or null if the MSA ID is not found.
-   */
-  async getMsaByPublicKey(publicKey: string): Promise<string | null> {
-    const cachedResult = this.cachePublicKeys.get(publicKey);
-    if (cachedResult && cachedResult.added.getTime() + 360 < new Date().getTime()) {
-      return cachedResult.msaId;
-    }
-
-    // TODO: Implement this
-    // Use Account Service to get the MSA ID
-    // try {
-    //   const response = await AccountService.getInstance().then((service) =>
-    //     service.getAccount(publicKey),
-    //   );
-    // } catch (e) {
-    //   console.error("Failed to get MSA ID: ", e);
-    //   return null;
-    // }
-
-    const api = await getApi();
-    const msaId = await api.query.msa.publicKeyToMsaId(publicKey);
-    if (msaId.isNone) return null;
-    const msaIdStr = msaId.value.toString();
-    this.cachePublicKeys.set(publicKey, { added: new Date(), msaId: msaIdStr });
-    return msaIdStr;
-  }
-
-  /**
-   * Retrieves the authentication token from the request headers.
-   * @param req - The request object with authentication token in the headers.
-   * @returns The authentication token or null if not found.
-   */
-  getAuthToken(req: Request): string | null {
-    if (req.headers?.authorization && typeof req.headers.authorization === 'string') {
-      return req.headers.authorization.split(' ')[1];
-    }
-    return null;
-  }
+/**
+ * Revokes an authentication token.
+ * @param token - The authentication token to revoke.
+ */
+export function revokeAuthToken(token: string): void {
+  authTokenRegistry.delete(token);
 }
 
-// Singleton instance of the TokenManager
-const tokenManager = new TokenManager();
+/**
+ * Retrieves the account associated with the provided authentication token.
+ * @param token - The authentication token.
+ * @returns A Promise that resolves to the account associated with the token, or null if no account is found.
+ */
+export async function getAccountFromAuth(token: string): Promise<null | RequestAccount> {
+  const account = authTokenRegistry.get(token);
+  if (!account) return null;
+  if (account.msaId) return account;
+
+  // Cache that msaId if we can...
+  account.msaId = (await getMsaByPublicKey(account.publicKey)) || undefined;
+  authTokenRegistry.set(token, account);
+  return account;
+}
+
+/**
+ * Retrieves the MSA ID associated with the given public key.
+ * @param publicKey - The public key for which to retrieve the MSA ID.
+ * @returns A Promise that resolves to the MSA ID as a string, or null if the MSA ID is not found.
+ */
+export async function getMsaByPublicKey(publicKey: string): Promise<string | null> {
+  const cachedResult = cachePublicKeys.get(publicKey);
+  if (cachedResult && cachedResult.added.getTime() + 360 < new Date().getTime()) {
+    return cachedResult.msaId;
+  }
+
+  // TODO: Implement this
+  // Use Account Service to get the MSA ID
+  // try {
+  //   const response = await AccountService.getInstance().then((service) =>
+  //     service.getAccount(publicKey),
+  //   );
+  // } catch (e) {
+  //   console.error("Failed to get MSA ID: ", e);
+  //   return null;
+  // }
+
+  const api = await getApi();
+  const msaId = await api.query.msa.publicKeyToMsaId(publicKey);
+  if (msaId.isNone) return null;
+  const msaIdStr = msaId.value.toString();
+  cachePublicKeys.set(publicKey, { added: new Date(), msaId: msaIdStr });
+  return msaIdStr;
+}
+
+/**
+ * Retrieves the authentication token from the request headers.
+ * @param req - The request object with authentication token in the headers.
+ * @returns The authentication token or null if not found.
+ */
+export function getAuthToken(req: Request): string | null {
+  if (req.headers?.authorization && typeof req.headers.authorization === 'string') {
+    return req.headers.authorization.split(' ')[1];
+  }
+
+  return null;
+}
 
 /**
  * Validates the authentication token in the request headers.
@@ -103,20 +100,20 @@ const tokenManager = new TokenManager();
  * @param next - The next middleware function.
  */
 export async function validateAuthToken(req: Request, res: Response, next: NextFunction) {
-  const token = tokenManager.getAuthToken(req);
+  const token = getAuthToken(req);
   logger.debug(`TokenAuth: validateAuthToken: token:(${token})`);
   if (!token) {
     return res.status(HttpStatusCode.Unauthorized).end();
   }
 
-  const account = await tokenManager.getAccountFromAuth(token);
+  const account = await getAccountFromAuth(token);
   logger.debug(`TokenAuth: validateAuthToken: account:(${JSON.stringify(account, null, 2)})`);
 
   if (account === null) {
     return res.status(HttpStatusCode.Unauthorized).end();
   }
 
-  // You can also add any auth information to the return value
+  // you can also add any auth information to the return value
   Object.assign(req.headers, account);
   next();
 }
