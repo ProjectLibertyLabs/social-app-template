ddbf203bd8a0cf1aea649eee337c90fce1ba30f4
Matthew Orris <1466844+mattheworris@users.noreply.github.com>
2024-06-17 14:45:48 -0400

    feat: add scripts to help clean up docker stuff during testing (#67)
    # Purpose
    The goal of this PR is add helper scripts to improve testing workflow.
    
    ## Solution
    
    Implemented shell script `restart.sh` that restarts the container setup
    and removes the persistent storage volumes. Handy when
    
    
    Implemented shell script `docker-clean-up.sh` to remove all container
    images, handy when environment variables change and the containers need
    to be re-started to pick up changes.

---
+++ backend/scripts/docker-clean-up.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Stop and remove containers, networks
+echo "Stopping and removing containers, networks..."
+docker compose --profile full down
+
+# Remove specified volumes
+echo "Removing specified volumes..."
+docker volume rm backend_redis_data
+docker volume rm backend_chainstorage
+docker volume rm backend_ipfs_data
+docker volume rm backend_node_cache
+
+# Remove all images
+echo "Removing all images..."
+docker rmi social-app-backend
+docker rmi amplicalabs/account-service
+docker rmi amplicalabs/content-watcher-service
+docker rmi amplicalabs/graph-service
+docker rmi amplicalabs/content-publishing-service
