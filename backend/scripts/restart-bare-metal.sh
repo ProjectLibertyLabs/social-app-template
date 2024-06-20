f3d9d8e17f379189f9f02610af809a1295c0741f
Matthew Orris <1466844+mattheworris@users.noreply.github.com>
2024-06-20 17:08:04 -0600

    feat: Add docker-compose-bare-metal.yaml for backend services

---
+++ backend/scripts/restart-bare-metal.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Stop and remove containers, networks
+echo "Stopping and removing containers, networks..."
+docker compose down
+
+# Remove specified volumes
+echo "Removing specified volumes..."
+docker volume rm backend_redis_data
+docker volume rm backend_chainstorage
+docker volume rm backend_ipfs_data
+docker volume rm backend_node_cache
+
+# Start specific services in detached mode
+echo "Starting frequency services..."
+docker compose -f docker-compose-bare-metal.yaml up -d frequency
+
+# Wait for 15 seconds
+echo "Waiting 15 seconds for Frequency to be ready..."
+sleep 15
+
+# Run npm run local:init 
+echo "Running npm run local:init to provision Provider with capacity, etc..."
+npm run local:init
+
+# Start all services in detached mode
+echo "Starting all services..."
+docker compose -f docker-compose-bare-metal.yaml up -d
