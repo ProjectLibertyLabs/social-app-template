02ed44a128f888e39d3cf727385b5de60df067c1
shannonwells <shannonwells@users.noreply.github.com>
2025-05-22 10:31:14 -0700

    npm audit updates and some logging

--- backend/scripts/local-init.cjs
+++ backend/scripts/local-init.cjs
@@ -19,6 +19,9 @@ const main = async () => {
     throwOnConnect: true,
     ...options,
   });
+  console.warn("awaiting isReady");
+  await api.isReady;
+  console.warn("I am ready");
   const keys = new Keyring().addFromUri('//Alice', {}, 'sr25519');
 
   // Create alice msa
